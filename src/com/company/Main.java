1bb48ab8a5d66ec24eb826857f1cdfc8ff0b0d15
Alin <alieenyoo@yahoo.com>
2022-05-03 18:48:44 +0300

    max

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please input a number");
+        int number=sc.nextInt();
+        int max=0;
+        if(number >0);
+        max=number;
+        System.out.println(number);
+    }
+}
